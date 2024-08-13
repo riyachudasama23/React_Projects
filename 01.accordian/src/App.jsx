aaf6839bc02c7783458b1d8d2d600d4f90c7d439
Riya <riyachudasama1123@gmail.com>
2024-08-13 23:36:52 +0530

    single selection

--- 01.accordian/src/App.jsx
+++ 01.accordian/src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   const [selected, setSelected] = useState(null);
 
   function handleSingleSelection(getCurrentId) {
-    console.log(getCurrentId);
+    //setSelected(getCurrentId);
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+    //open and close single selection
   }
+  console.log(selected);
 
   return (
     <div className="wrapper">
@@ -21,8 +24,11 @@ function App() {
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
-                <span>+</span>
+                <span>v</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
